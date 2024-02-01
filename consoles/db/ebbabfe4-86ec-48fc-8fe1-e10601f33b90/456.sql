f4175cb0c74c27fd83c3ff95367b15350673daa2
mytoroto <lvjie1993@gmail.com>
2024-02-01 14:22:26 +0800

    Changes

---
+++ consoles/db/ebbabfe4-86ec-48fc-8fe1-e10601f33b90/456.sql
@@ -0,0 +1,7 @@
+select
+    a.name as `name`,
+    a.id as `id`,
+    a.age as `age`,
+    a.address as `address`,
+    a.gender as `gender`,
+    a.salary as `salary`
--- consoles/db/ebbabfe4-86ec-48fc-8fe1-e10601f33b90/其他/console.sql
+++ consoles/db/ebbabfe4-86ec-48fc-8fe1-e10601f33b90/其他/console.sql
@@ -50,3 +50,47 @@ left join dwm.dwd_dim_dict ddd on ddd.element = t2.route_action and ddd.db = 'my
 left join my_staging.order_info oi on oi.pno = t1.pno
 where
     if(t2.routed_at is not null ,t2.routed_at > date_sub(t1.updated_at, interval 8 hour), 1 = 1)
+
+;
+
+
+SELECT
+  date(a.`created_at`) as '日期',
+  COUNT(1) AS 'C来源抓取数量',
+  SUM(IF(b.`id` IS NOT NULL, 1, 0)) AS '上报有发无到数量',
+  SUM(
+    IF(
+      a.`state` = 5
+      AND DATE(a.`created_at`) = DATE(a.`updated_at`),
+      1,
+      0
+    )
+  ) AS '当日无责数量',
+  SUM(
+    IF(
+      a.`state` = 5
+      AND DATE_ADD(DATE(a.`created_at`), 1) = DATE(a.`updated_at`),
+      1,
+      0
+    )
+  ) AS '次日无责数量',
+
+  SUM(
+    IF(
+      a.`state` = 5
+      AND a.`updated_at` < DATE_ADD(a.`created_at`, 1),
+      1,
+      0
+    )
+  ) AS '24小内无责量'
+FROM
+  my_bi.`parcel_lose_task` a
+  LEFT JOIN `my_staging`.`parcel_route` b ON a.pno = b.pno
+  AND b.`route_action` = 'HAVE_HAIR_SCAN_NO_TO'
+  AND DATE(CONVERT_TZ(b.`routed_at`, '+00:00', '+07:00')) = DATE(a.`created_at`)
+WHERE
+  a.`created_at` BETWEEN '2024-01-20 00:00:00' AND '2024-02-01 00:00:00'
+  AND a.`source` = 3
+group by
+  日期
+ORDER BY 日期 DESC
